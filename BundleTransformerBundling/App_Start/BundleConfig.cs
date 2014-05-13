cc179eddaa67c1401e06f7083d700ad715cf6f61
Andrew Hughson <andrew@hughson.me>
2014-05-13 22:45:27 +1200

    Add bundletransform bundling

---
+++ BundleTransformerBundling/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Optimization;
+using BundleTransformer.Core.Transformers;
+
+namespace BundleTransformerBundling
+{
+    public class BundleConfig
+    {
+        public const string JavaScript = "~/bundles/javascript";
+        public const string Styles = "~/bundles/styles";
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var stylesBundle = new Bundle(Styles);
+            stylesBundle.Include("~/Styles/Site.less");
+            stylesBundle.Transforms.Add(new CssTransformer());
+            stylesBundle.Transforms.Add(new CssMinify());
+
+            bundles.Add(stylesBundle);
+        }
+    }
+}
